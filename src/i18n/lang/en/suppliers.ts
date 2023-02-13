d6a63f574aa1dc0ede28c2492e6f26ad72ff7805
KostrebaDV <kostrebadd@gmail.com>
2023-02-14 00:09:18 +0200

    EA-86 Create error translations for Supplier module (#14)

--- src/i18n/lang/en/suppliers.ts
+++ src/i18n/lang/en/suppliers.ts
@@ -1,9 +1,11 @@
 export const suppliers = {
   suppliers: 'Suppliers',
   'suppliers.name': 'Supplier name',
+  'suppliers.name.error.required': 'Supplier name is required',
   'suppliers.account': 'Account',
   'suppliers.phone': 'Phone',
+  'suppliers.phone.error.length': 'Phone number should be equal 10 digits',
   'suppliers.note': 'Note',
+  'suppliers.note.error.max': 'Note should be less than 1024 characters',
   'suppliers.bill': 'Bill',
-  'suppliers.phoneNumber': 'Supplier phone',
 };
--- src/i18n/lang/uk/suppliers.ts
+++ src/i18n/lang/uk/suppliers.ts
@@ -1,9 +1,11 @@
 export const suppliers = {
   suppliers: 'Постачальники',
   'suppliers.name': 'Назва постачальника',
+  'suppliers.name.error.required': "Назва постачальника є обов'язковою",
   'suppliers.account': 'Рахунок постачальника',
   'suppliers.phone': 'Телефон',
+  'suppliers.phone.error.length': 'Номер телефона має містити 10 цифр',
   'suppliers.note': 'Нотатки',
+  'suppliers.note.error.max': 'Нотатка має містити не більше 1024 символів',
   'suppliers.bill': 'Рахунок',
-  'suppliers.phoneNumber': 'Номер телефона',
 };
